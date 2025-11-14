b895c5a341809bbb9820aa626e33bdc994c7705b
QuilexDev <xmanueldls4@gmail.com>
2025-11-13 21:39:45 -0400

    scrolling

--- js/script.js
+++ js/script.js
@@ -7,6 +7,26 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+///////////////////////////////////////////////////////////
+//Smooth scrolling animation
+const alllinks = document.querySelectorAll("a:link");
+alllinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
